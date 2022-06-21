eb7ba09fb99933ab35abbdec27de31eacac17f42
iskinner21 <iskinner@udesa.edu.ar>
2022-06-21 13:45:47 -0300

    termine detail-artist

--- js/detail-artist.js
+++ js/detail-artist.js
@@ -15,20 +15,30 @@ fetch (url1)
 
         let title = document.querySelector ('h1');
         let imgAlbum = document.querySelector ('.imgda');
-        let temasAlbum = document.querySelector ('.contenedorpadre');
+        let temasAlbum = document.querySelector ('.cancionda');
 
         title.innerText = data.name;
-        imgAlbum.innerText =  `<img src="${info[i].picture}" alt="${data[i].name}" class="imgda"></img>`  
-        temasAlbum.innerText = data.tracks        
-
-        
-
-    
+        imgAlbum.src =  data.picture 
 
     })
     .catch(function(error){
         console.log(error);
     })
 
+    let urlCanciones = `https://api.allorigins.win/raw?url=https://api.deezer.com/artist/${id}/albums`
 
+    fetch(urlCanciones)
+    .then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        console.log(data);
+    
+        let detalle = document.querySelector('.detalles1');
+        for (let i = 0; i <6; i++) {
+        detalle.innerHTML +=`<a href="./detail-album.html?id=${data.data[i].id}"> <li class="albums">${data.data[i].title}</li> </a>`
+        }
+    
+    }).catch(function(errores) {
+      console.log(errores);  
+    })
 
